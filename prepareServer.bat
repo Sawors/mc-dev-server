0aadc96c5522822137b3f4dde2d03dc53ea7949f
Sawors <40705096+Sawors@users.noreply.github.com>
2023-05-18 20:15:04 +0200

    Add files via upload

---
+++ prepareServer.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+set source=%CD%
+set server="%source%\_server\server"
+set source_name=%source%
+for %%* in (.) do set source_name=%%~n*
+
+rem usually you shouldn't have to edit these
+set javah=%JAVA_HOME%
+set server_executable="paper"
+IF NOT "%~1"=="" set server_executable=%1
+set ram=4
+
+rem clean + package the project
+call mvn clean
+call mvn package
+rem move the plugin the the server
+del /q "%source%\target\%source_name%-*-shaded.jar"
+del /q "%server%\plugins\%source_name%-*.jar"
+xcopy "%source%\target\%source_name%-*.jar" "%server%\plugins" /y /s
+rem start the server
+rem cd "%server%\"
+rem "%javah%\bin\java" -Xmx%ram%G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar %server_executable%.jar nogui
