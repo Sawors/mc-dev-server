c42b9b22f2610f6eff8bf5b7215282877b48daf2
Sawors <40705096+Sawors@users.noreply.github.com>
2023-06-02 18:21:19 +0200

    Add files via upload

--- prepareServer.bat
+++ prepareServer.bat
@@ -20,4 +20,4 @@ del /q "%server%\plugins\%source_name%-*.jar"
 xcopy "%source%\target\%source_name%-*.jar" "%server%\plugins" /y /s
 rem start the server
 rem cd "%server%\"
-rem "%javah%\bin\java" -Xmx%ram%G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar %server_executable%.jar nogui
+"%javah%\bin\java" -Xmx%ram%G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar %server_executable%.jar nogui
